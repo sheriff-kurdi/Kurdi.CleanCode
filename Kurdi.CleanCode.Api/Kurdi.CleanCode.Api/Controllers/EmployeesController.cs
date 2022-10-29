19bbc6c345129545bb508e37f6438380b78f1a31
kurdi <sheriff.kurdi@gmail.com>
2022-10-29 14:40:40 +0100

    .net 6 api

---
+++ Kurdi.CleanCode.Api/Kurdi.CleanCode.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,84 @@
+
+using AutoMapper;
+using Kurdi.CleanCode.Core.Entities;
+using Kurdi.CleanCode.Infrastructure.DTOs;
+using Kurdi.CleanCode.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Kurdi.CleanCode.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+
+        private readonly IEmployeeService employeesService;
+        private readonly IMapper mapper;
+
+        public EmployeesController(IEmployeeService employeesService, IMapper mapper)
+        {
+            this.employeesService = employeesService;
+            this.mapper = mapper;
+        }
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public List<EmployeeDTO> Get([FromQuery] int pageNumber, [FromQuery] int pageSize)
+        {
+            List<EmployeeDTO> emps = new List<EmployeeDTO>();
+            foreach (var emp in employeesService.FindAll(pageSize, pageNumber))
+            {
+                emps.Add(mapper.Map<EmployeeDTO>(emp));
+            
+            }
+            //TODO why not working
+            //IQueryable < EmployeeDTO > emps = mapper.Map<IQueryable<EmployeeDTO>>(employeesService.FindAll(pageSize, pageNumber));
+            return emps;
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id, [FromQuery] int pageSize, [FromQuery] int pageNumber)
+        {
+            
+            return employeesService.FindByCondition(e => e.Id == id, pageSize, pageNumber).FirstOrDefault();
+        }
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            employeesService.Create(employee);
+        }
+
+        // PUT api/<EmployeesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Employee employee, [FromQuery] int pageSize, [FromQuery] int pageNumber)
+        {
+            if (employeesService.FindByCondition(e => e.Id == id, pageSize, pageNumber).FirstOrDefault() == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (employeesService.FindByCondition(e => e.Id == id).FirstOrDefault() == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+
+        }
+    }
+}
